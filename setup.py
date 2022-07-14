e8fdf76f66c198de7fcee9032b66fff73947f063
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2022-07-14 12:19:45 +0200

    hello world

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='blind docking of putative SIRT3 small molecule activators using equibind and vina',
+    author='Niklas Rindtorff',
+    license='',
+)
